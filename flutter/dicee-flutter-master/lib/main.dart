0903904ac33e2217ddd75ea463df711a691a4648
nilsbruhn1702 <113424098+nilsbruhn1702@users.noreply.github.com>
2022-09-17 15:27:37 +0200

    no message

--- flutter/dicee-flutter-master/lib/main.dart
+++ flutter/dicee-flutter-master/lib/main.dart
@@ -18,6 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  print('linker Button gedrückt');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  print('rechter Button gedrückt');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
