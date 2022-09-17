589dbe3e29dea43fa0daa54736a53514f4045dce
nilsbruhn1702 <113424098+nilsbruhn1702@users.noreply.github.com>
2022-09-17 13:38:02 +0200

    finished flutter course part 40-50

--- dart/Basics/BasicsProgram.dart
+++ dart/Basics/BasicsProgram.dart
@@ -1,13 +1,18 @@
 import 'dart:math';
 
 class Dog {
-  
-  late int legs;
-  late int eyes;
-  late String name;
+
   int _weight = 30;
   
-  Dog({required this.name, required this.eyes, required this.legs});
+  var eyes;
+  
+  var legs;
+  
+  var name;
+  
+  String speed;
+  
+  Dog(String this.speed, {required this.name, required this.eyes, required this.legs});
 
   int allBodyparts(){
     return eyes + legs;
@@ -19,7 +24,7 @@ class Dog {
 }
 
 void main(){
-  var Jimmy = Dog(name: 'Jimmy', eyes: 2, legs: 4);
+  var Jimmy = Dog('schnell', name: 'Jimmy', eyes: 2, legs: 4);
   print(Jimmy.allBodyparts());
   Jimmy.printDogsBodypartsAndEyes();
   print(Jimmy._weight);
--- flutter/flutter_application_1/lib/main.dart
+++ flutter/flutter_application_1/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+double sizedBoxHeight = 5;
+double paddingValue = 10;
+double marginValue = 20;
+double sizedBoxWidth = 10;
 void main() {
   runApp(MyApp());
 }
@@ -8,7 +12,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: 
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg'),
+              ),
+              SizedBox(
+                height: sizedBoxHeight,
+              ),
+              Text('Manfred Müller'),
+              Text(
+                'Project Manager'.toUpperCase(),
+                style: TextStyle(fontSize: 10.0, fontFamily: 'Oswald'),
+              ),
+              SizedBox(height: sizedBoxHeight),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                color: Colors.white,
+                child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text('09834 343424'))),
+              ),
+              SizedBox(height: sizedBoxHeight),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.email),
+                        SizedBox(width: sizedBoxWidth),
+                        Text('maxmustermann@gmail.com')
+                      ]),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
