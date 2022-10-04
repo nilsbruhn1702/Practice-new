5ba5eff565740d9f22550f38c27ed7054b176eea
nilsbruhn1702 <113424098+nilsbruhn1702@users.noreply.github.com>
2022-10-04 12:30:30 +0200

    no message

--- flutter/pokemon_app/lib/Screens/pokemon_card_view/pokemon_card_view.dart
+++ flutter/pokemon_app/lib/Screens/pokemon_card_view/pokemon_card_view.dart
@@ -56,8 +56,6 @@ class _PokemonCardViewState extends State<PokemonCardView> {
         Provider.of<PokemonData>(context, listen: true).turn == false &&
             Provider.of<PokemonData>(context, listen: true).player ==
                 Player.playerTwo) {
-      print(Provider.of<PokemonData>(context, listen: true).turn);
-      print(Provider.of<PokemonData>(context, listen: true).player);
       return Scaffold(
         appBar: AppBar(
           bottom: PreferredSize(child: Text(''), preferredSize: Size(0, 20)),
@@ -70,51 +68,88 @@ class _PokemonCardViewState extends State<PokemonCardView> {
           ),
           title: PokemonText(text: 'Pokemon Quartett'),
         ),
-        body: FutureBuilder(
-            future: getPokemonInstance(),
-            builder: (context, snapshot) {
-              if ((snapshot.connectionState == ConnectionState.done) &&
-                  (snapshot.data != null)) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 195, 98, 0),
-                            borderRadius: BorderRadius.circular(18)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(
-                            'Cards left : ${cardsLeft.toString()}',
-                            style: TextStyle(
-                              fontFamily: 'Silkscreen',
-                              fontSize: 20,
-                            ),
-                          ),
+        body: PokemonGameView(),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(child: Text(''), preferredSize: Size(0, 20)),
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Image(
+                image: AssetImage('images/Pokeball-PNG-Photo-Image.png'),
+                fit: BoxFit.contain),
+          ),
+          title: PokemonText(text: 'Pokemon Quartett'),
+        ),
+        body: Stack(children: [
+          PokemonGameView(),
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(136, 0, 0, 0),
+            ),
+            height: double.infinity,
+            width: double.infinity,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(
+                  0, 0, 0, MediaQuery.of(context).size.height - 200),
+              decoration: BoxDecoration(color: Color.fromARGB(152, 0, 0, 0)),
+              child: Text(
+                "It's not your turn, please wait!",
+                style: TextStyle(
+                    fontFamily: 'Silkscreen',
+                    color: Colors.white,
+                    fontSize: 20,
+                    decoration: TextDecoration.none),
+              ),
+            ),
+          )
+        ]),
+      );
+    }
+  }
+
+  FutureBuilder<List<dynamic>> PokemonGameView() {
+    return FutureBuilder(
+        future: getPokemonInstance(),
+        builder: (context, snapshot) {
+          if ((snapshot.connectionState == ConnectionState.done) &&
+              (snapshot.data != null)) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 195, 98, 0),
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Cards left : ${cardsLeft.toString()}',
+                        style: TextStyle(
+                          fontFamily: 'Silkscreen',
+                          fontSize: 20,
                         ),
                       ),
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width - 10,
-                      height: MediaQuery.of(context).size.height - 230,
-                      child: Stack(
-                        children: [
-                          PokemonCard(PokemonInstance.get(snapshot.data!)),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return PokemonCardLoadingView();
-              }
-            }),
-      );
-    } else {
-      print(Provider.of<PokemonData>(context, listen: true).turn);
-      print(Provider.of<PokemonData>(context, listen: true).player);
-      return Container();
-    }
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: MediaQuery.of(context).size.height - 230,
+                  child: Stack(
+                    children: [
+                      PokemonCard(PokemonInstance.get(snapshot.data!)),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return PokemonCardLoadingView();
+          }
+        });
   }
 }
